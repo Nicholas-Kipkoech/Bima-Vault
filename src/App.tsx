b72d23d304ce5ebfd5ee99178577ac2d20641c84
Nicholas Kipkoech <nickey968@gmail.com>
2024-09-09 12:20:47 +0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div className="text-red-700">
+      <Button>Nicholas</Button>
+    </div>
+  )
+}
+
+export default App
